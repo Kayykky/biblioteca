95ec377c3b0d010ad2f0a75d59ecd3f23c05aa7f
Kayky <125319686+Kayykky@users.noreply.github.com>
2023-03-04 15:28:21 -0300

    Update novo-func.php

--- biblioteca/principal/func/novo-func.php
+++ biblioteca/principal/func/novo-func.php
@@ -4,13 +4,13 @@
 		<input type="hidden" name="acao" value="cadastrar">
 
 		<label>Nome:</label><br>
-		<input type="text" name="nome"><br>
+		<input type="text" name="nome" required><br>
 	
 		<label>Login:</label><br>
-		<input type="text" name="login"><br>
+		<input type="text" name="login" required><br>
 
 		<label>Senha:</label><br>
-		<input type="text" name="senha"><br><br>
+		<input type="text" name="senha" required><br><br>
 	
 		<button type="submit" class="button">Enviar</button>
 	</form>
